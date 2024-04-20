cb935e6bd534b699f4b4e20b35e7bb83f29803d9
unknown <rubendorado@unicauca.edu.co>
2024-04-20 18:59:40 -0500

    Se agrega funcionalidad de eliminar reporte

--- main.py
+++ main.py
@@ -1,75 +1,49 @@
-import numpy as np
 from flask import Flask
 from flask import jsonify
-from datetime import datetime
+import json
 
 app = Flask(__name__)
 
 BASE_URL = 'reqsmells'
 PORT = 8080
 
-reports_resume = [
-    {
-        'report_id': 1,
-        'report_name': 'Evaluación de ejemplo # 1',
-        'report_date': datetime(2024, 4, 10),
-        'report_overall_score': 75.12,
-        'report_requirements': [ 
-            { 'req_id': 1, 'req_code': 'HU001', 'req_name': 'Inicio de sesión' },
-            { 'req_id': 2, 'req_code': 'HU002', 'req_name': 'Cerrar sesión' },
-            { 'req_id': 3, 'req_code': 'HU003', 'req_name': 'registro' }
-        ]
-    },
-    {
-        'report_id': 2,
-        'report_name': 'Evaluación de ejemplo # 2',
-        'report_date': datetime(2024, 4, 11),
-        'report_overall_score': 70.12,
-        'report_requirements': [ 
-            { 'req_id': 1, 'req_code': 'HU001', 'req_name': 'Inicio de sesión' }
-        ]
-    },
-    {
-        'report_id': 3,
-        'report_name': 'Evaluación de ejemplo # 3',
-        'report_date': datetime(2024, 4, 11),
-        'report_overall_score': 10.12,
-        'report_requirements': [ 
-            { 'req_id': 1, 'req_code': 'HU001', 'req_name': 'Inicio de sesión' },
-            { 'req_id': 2, 'req_code': 'HU002', 'req_name': 'Cerrar sesión' },
-            { 'req_id': 3, 'req_code': 'HU003', 'req_name': 'registro' },
-            { 'req_id': 4, 'req_code': 'HU004', 'req_name': 'Mostrar productos' },
-            { 'req_id': 5, 'req_code': 'HU005', 'req_name': 'Eliminar producto' }
-        ]
-    },
-    {
-        'report_id': 4,
-        'report_name': 'Evaluación de ejemplo # 4',
-        'report_date': datetime(2024, 4, 12),
-        'report_overall_score': 20.12,
-        'report_requirements': [ 
-            { 'req_id': 1, 'req_code': 'HU001', 'req_name': 'Inicio de sesión' },
-            { 'req_id': 2, 'req_code': 'HU002', 'req_name': 'Cerrar sesión' },
-            { 'req_id': 3, 'req_code': 'HU003', 'req_name': 'registro' }
-        ]
-    },
-    {
-        'report_id': 5,
-        'report_name': 'Evaluación de ejemplo # 5',
-        'report_date': datetime(2024, 4, 10),
-        'report_overall_score': 40.12,
-        'report_requirements': [ 
-            { 'req_id': 1, 'req_code': 'HU001', 'req_name': 'Inicio de sesión' },
-            { 'req_id': 2, 'req_code': 'HU002', 'req_name': 'Cerrar sesión' },
-            { 'req_id': 3, 'req_code': 'HU003', 'req_name': 'registro' }
-        ]
-    }
-]
+# json files
+reports_resume_url = './data/reports-resume.json'
+
 
 # Listado de reportes
 @app.route(f'/{BASE_URL}/get-reports-resume', methods=['GET'])
-def predict():    
+def get_reports_resume(): 
+    reports_resume = read_json('./data/reports-resume.json')       
     return jsonify({'reports' : reports_resume})
+
+
+# Eliminar reporte
+@app.route(f'/{BASE_URL}/delete-report/<int:report_id>', methods=['DELETE'])
+def delete_report(report_id):    
+    reports_resume = read_json(reports_resume_url)
+    previous_len = len(reports_resume)
+
+    # delete 
+    reports_resume = [d for d in reports_resume if d['report_id'] != report_id]
+    write_json(reports_resume_url, reports_resume)
+
+    # prepare response
+    report_found = len(reports_resume) < previous_len
+    response_message = 'Registro eliminado exitosamente' if report_found else 'No se pudo encontrar reporte para eliminar'
+    status = 200 if report_found else 404    
+
+    return jsonify({'message' : response_message}), status
+
+
+def read_json(url: str):
+    with open(url, 'r', encoding='utf-8') as data_file:
+        return json.load(data_file)
+    
+
+def write_json(url: str, data:list):
+    with open(url, 'w', encoding='utf-8') as data_file:
+        json.dump(data, data_file, indent=4)
 
 
 if __name__ == "__main__":
