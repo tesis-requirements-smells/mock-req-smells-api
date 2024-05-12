df3f0f1d327bffb1722bf06e6296c3bbf0f27a36
unknown <rubendorado@unicauca.edu.co>
2024-05-11 19:37:43 -0500

    Ajuste en respuesta del evaluar datos

--- main.py
+++ main.py
@@ -245,7 +245,8 @@ def evaluate_data():
         "report_name": report_name,
         "report_date": get_current_date(),
         "report_overall_score": round(random.uniform(0, 100), 2),
-        "results_by_requirement": evaluation
+        "results_by_requirement": evaluation['results_by_requirement'],
+        "global_results": evaluation['global_results']
     }
     reports_resume.append(new_report)
     write_json(REPORTS_URL, reports_resume)
